ab5ffa44a9d515853125696e4acf74a20152812c
David Brockwell <dbrockwell@my.wctc.edu>
2022-05-04 23:34:31 -0500

    added updating products, modified InputProducts

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace FinalProject
                 if (choose == "1") 
                 {
                     var db = new NWConsole_48_DABContext();
-                    Product product = InputProduct(db);
+                    Product product = InputProduct(db, 0);
                     if (product != null) {
                        db.AddProduct(product);
                        //Console.WriteLine(product.SupplierId);
@@ -44,7 +44,40 @@ namespace FinalProject
                 }
                 else if (choose == "2")
                 {
-
+                    var db = new NWConsole_48_DABContext();
+                    bool loop = false;
+                    Product product = null;
+                    do {
+                        Console.WriteLine("Choose a product for editing:");
+                        ShowProducts(db);
+                        try{
+                            int productIdWrite = int.Parse(Console.ReadLine());
+                            if (db.Products.Any(p => p.ProductId == productIdWrite)) {
+                                product = db.Products.FirstOrDefault(p => p.ProductId == productIdWrite);
+                                loop = false;
+                            }
+                            else {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                logger.Error("ProductId does not exist");
+                                Console.ForegroundColor = ConsoleColor.White;
+                                loop = true;
+                            }
+                        } catch (Exception) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            logger.Error("Whole Number was not entered");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            loop = true;
+                        }
+                    } while (loop == true);
+                    if (product != null) {
+                        int productId = product.ProductId;
+                        Product updatedProduct = InputProduct(db, productId);
+                        if (product != null) {
+                            updatedProduct.ProductId = product.ProductId;
+                            db.EditProduct(updatedProduct);
+                            logger.Info($"Product (id: {product.ProductId}) updated");
+                        }
+                    }
                 }
                 else if (choose == "3")
                 {
@@ -112,95 +145,196 @@ namespace FinalProject
             Console.ForegroundColor = ConsoleColor.White;
         }
 
-        public static Product InputProduct(NWConsole_48_DABContext db){
+        public static Product InputProduct(NWConsole_48_DABContext db, int productId){
             Product product = new Product();
+            Product currentProduct = null;
             bool loop = false;
+            if (productId != 0) {
+                currentProduct = db.Products.FirstOrDefault(p => p.ProductId == productId);
+                Console.WriteLine("Press enter to skip any fields");
+            }
             Console.WriteLine("Enter Product name:");
-            product.ProductName = Console.ReadLine();
-            if (product.ProductName != "") {
-                do{
-                Console.WriteLine("Enter Supplier ID:");
-                ShowSuppliers(db);
-                try {
-                product.SupplierId = int.Parse(Console.ReadLine());
-                loop = false;
-                } catch (Exception) {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    logger.Error("Whole Number was not entered");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    loop = true;
+            if (currentProduct != null) {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Current Product Name: {currentProduct.ProductName} [press \"Enter\" to use]");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            string productName = Console.ReadLine();
+            if (product.ProductName != "" || productId != 0) {
+                if (currentProduct != null && productName == "") {
+                    product.ProductName = currentProduct.ProductName;
                 }
+                else {
+                    product.ProductName = productName;
+                }
+                do{
+                    Console.WriteLine("Enter Supplier ID:");
+                    ShowSuppliers(db);
+                    if (currentProduct != null) {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Current Product Supplier ID: {currentProduct.SupplierId} [press \"Enter\" to use]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    string supplierId = Console.ReadLine();
+                    if (currentProduct != null && supplierId == "") {
+                        product.SupplierId = currentProduct.SupplierId;
+                    }
+                    else {
+                        try {
+                            product.SupplierId = int.Parse(supplierId);
+                            loop = false;
+                        } catch (Exception) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            logger.Error("Whole Number was not entered");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            loop = true;
+                        }
+                    }
                 } while(loop == true);
                 do{
-                Console.WriteLine("Enter Category ID:");
-                ShowCategories(db);
-                try {
-                product.CategoryId = int.Parse(Console.ReadLine());
-                loop = false;
-                } catch (Exception) {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    logger.Error("Whole Number was not entered");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    loop = true;
-                }
+                    Console.WriteLine("Enter Category ID:");
+                    ShowCategories(db);
+                    if (currentProduct != null) {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Current Product Category ID: {currentProduct.CategoryId} [press \"Enter\" to use]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    string categoryId = Console.ReadLine();
+                    if (currentProduct != null && categoryId == "") {
+                        product.CategoryId = currentProduct.CategoryId;
+                    }
+                    else{
+                        try {
+                            product.CategoryId = int.Parse(categoryId);
+                            loop = false;
+                        } catch (Exception) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            logger.Error("Whole Number was not entered");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            loop = true;
+                        }
+                    }
                 } while(loop == true);
                 Console.WriteLine("Enter Quantity per Unit:");
-                product.QuantityPerUnit = Console.ReadLine();
-                do{
-                Console.WriteLine("Enter Unit Price:");
-                try {
-                product.UnitPrice = decimal.Parse(Console.ReadLine());
-                loop = false;
-                } catch (Exception) {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    logger.Error("Number was not entered");
+                if (currentProduct != null) {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Current Product Quantity Per Unit: {currentProduct.QuantityPerUnit} [press \"Enter\" to use]");
                     Console.ForegroundColor = ConsoleColor.White;
-                    loop = true;
                 }
+                string quantityPerUnit = Console.ReadLine();
+                if (currentProduct != null && quantityPerUnit == "") {
+                    product.QuantityPerUnit = currentProduct.QuantityPerUnit;
+                }
+                else {
+                    product.QuantityPerUnit = quantityPerUnit;
+                }
+                do{
+                    Console.WriteLine("Enter Unit Price:");
+                    if (currentProduct != null) {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Current Product Unit Price: ${currentProduct.UnitPrice} [press \"Enter\" to use]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    string unitPrice = Console.ReadLine();
+                    if (currentProduct != null && unitPrice == "") {
+                        product.UnitPrice = currentProduct.UnitPrice;
+                    }
+                    else {
+                        try {
+                            product.UnitPrice = decimal.Parse(unitPrice);
+                            loop = false;
+                        } catch (Exception) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            logger.Error("Number was not entered");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            loop = true;
+                        }
+                    }
                 } while(loop == true);
                 do{
-                Console.WriteLine("Enter Units in Stock:");
-                try {
-                product.UnitsInStock = short.Parse(Console.ReadLine());
-                loop = false;
-                } catch (Exception) {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    logger.Error("Whole Number was not entered");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    loop = true;
-                }
+                    Console.WriteLine("Enter Units in Stock:");
+                    if (currentProduct != null) {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Current Product Units in Stock: {currentProduct.UnitsInStock} [press \"Enter\" to use]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    string unitsInStock = Console.ReadLine();
+                    if (currentProduct != null && unitsInStock == "") {
+                        product.UnitsInStock = currentProduct.UnitsInStock;
+                    }
+                    else {
+                        try {
+                            product.UnitsInStock = short.Parse(unitsInStock);
+                            loop = false;
+                        } catch (Exception) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            logger.Error("Whole Number was not entered");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            loop = true;
+                        }
+                    }
                 } while(loop == true);
                 do{
-                Console.WriteLine("Enter Units on Order:");
-                try {
-                product.UnitsOnOrder = short.Parse(Console.ReadLine());
-                loop = false;
-                } catch (Exception) {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    logger.Error("Whole Number was not entered");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    loop = true;
-                }
+                    Console.WriteLine("Enter Units on Order:");
+                    if (currentProduct != null) {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Current Product Units on Order: {currentProduct.UnitsOnOrder} [press \"Enter\" to use]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    string unitsOnOrder = Console.ReadLine();
+                    if (currentProduct != null && unitsOnOrder == "") {
+                        product.UnitsOnOrder = currentProduct.UnitsOnOrder;
+                    }
+                    else {
+                        try {
+                            product.UnitsOnOrder = short.Parse(unitsOnOrder);
+                            loop = false;
+                        } catch (Exception) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            logger.Error("Whole Number was not entered");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            loop = true;
+                        }
+                    }
                 } while(loop == true);
                 do{
-                Console.WriteLine("Enter Record Level:");
-                try {
-                product.ReorderLevel = short.Parse(Console.ReadLine());
-                loop = false;
-                } catch (Exception) {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    logger.Error("Whole Number was not entered");
-                    Console.ForegroundColor = ConsoleColor.White;
-                    loop = true;
-                }
+                    Console.WriteLine("Enter Recorder Level:");
+                    if (currentProduct != null) {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Current Product Recorder Level: {currentProduct.ReorderLevel} [press \"Enter\" to use]");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    string recorderLevel = Console.ReadLine();
+                    if (currentProduct != null && recorderLevel == "") {
+                        product.ReorderLevel = currentProduct.ReorderLevel;
+                    }
+                    else {
+                        try {
+                            product.ReorderLevel = short.Parse(recorderLevel);
+                            loop = false;
+                        } catch (Exception) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            logger.Error("Whole Number was not entered");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            loop = true;
+                        }
+                    }
                 } while(loop == true);
                 Console.WriteLine("Enter (1) if product is discontinued, or Enter (2) if product is not discontinued");
+                if (currentProduct != null) {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Current Product Discontinued Status: {currentProduct.Discontinued} [press \"Enter\" to use]");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
                 string discontinued = Console.ReadLine();
                 if (discontinued == "1") {
                     product.Discontinued = true;
                 }
                 else if (discontinued == "2") {
                     product.Discontinued = false;
+                }
+                else if (discontinued == "" && currentProduct != null) {
+                    product.Discontinued = currentProduct.Discontinued;
                 }
                 else {
                     product.Discontinued = false;
@@ -216,7 +350,7 @@ namespace FinalProject
                     bool validForeign = db.Suppliers.Any(s => s.SupplierId == product.SupplierId) && db.Categories.Any(c => c.CategoryId == product.CategoryId);
                     bool supplierValid = db.Suppliers.Any(s => s.SupplierId == product.SupplierId);
                     bool categoryValid = db.Categories.Any(c => c.CategoryId == product.CategoryId);
-                    if (db.Products.Any(p => p.ProductName == product.ProductName))
+                    if (db.Products.Any(p => p.ProductName == product.ProductName) && product.ProductName != currentProduct.ProductName)
                     {
                         isValid = false;
                         results.Add(new ValidationResult("Name exists", new string[] { "ProductName" }));
